071635627d1795bef81ff52d07ec54939642b0d3
kleeba119 <kleeba119@gmail.com>
2026-09-20 15:13:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import whisper
-from moviepy.editor import VideoFileClip, ImageClip, CompositeVideoClip
+from moviepy import VideoFileClip, ImageClip, CompositeVideoClip
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import os
